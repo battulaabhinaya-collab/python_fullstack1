1a57f365a9c88c23184eb4cfaa4e6a8e57057aed
battula bhava sai abhinaya <battulaabhinaya@gmail.com>
2026-08-13 10:19:40 +0000

    UPDATE

---
+++ test.js
@@ -0,0 +1,36 @@
+console.log("hello world");
+console.log(999+111);
+console.log("999"+"111");
+console.log(typeof("999"));
+console.log(typeof(999));
+console.log(parseInt("999"));
+console.log(parseFloat("999.99"));
+console.log(Number("999"));
+console.log(String(999));
+//comment
+let a=10;
+console.log(a);
+a=11;
+console.log(a);
+const pi = 3.14159;
+console.log(pi);
+//
+let b = 100;
+console.log(b);
+b = 200;
+console.log(b);
+let num = 10;
+if (num%2 == 0){
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
+for(i=0;i<10;i++){
+    console.log(i*2)
+}
+i = 0;
+while(i<0){
+    
+}
+
